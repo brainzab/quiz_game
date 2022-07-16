ceee3c3f83cb767ba3b9ac6f599e3e5876659ba7
brainzab <brainzab@gmail.com>
2022-07-16 03:17:03 +0300

    commit

---
+++ lib/question.rb
@@ -0,0 +1,15 @@
+class Question
+  attr_reader :text :variants :time_to_answer :points
+
+  def initialize(text, variants, time_to_answer, points)
+    @text = text
+    @variants = variants
+    @time_to_answer = time_to_answer
+    @points = points
+  end
+
+  def to_s
+    @variants.map.with_index(1) do |variant, index|
+      "#{index}. #{variant[1]}".join('\')
+    end
+  end
